703eab987137c28d80f496bdbc73477a77a7b808
Kimberly Garcés Pérez <kimberly.gp@hotmail.com>
2022-04-03 16:23:49 -0500

    final test

---
+++ src/main/java/co/com/choucair/certification/proyectoBase/model/ChoucairData.java
@@ -0,0 +1,4 @@
+package co.com.choucair.certification.proyectoBase.model;
+
+public class ChoucairData {
+}
---
+++ src/main/java/co/com/choucair/certification/proyectoBase/userinterface/PageFinal.java
@@ -0,0 +1,4 @@
+package co.com.choucair.certification.proyectoBase.userinterface;
+
+public class PageFinal {
+}
